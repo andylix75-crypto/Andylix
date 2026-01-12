d605475f02f323abec511d147defde1780e2a89f
200~aeenspd2022-lgtm <aeenspd2022@gmail.com>
2026-01-12 03:37:03 +0100

    ajout du module annonce

--- Backend/UserModule/routes/api.php
+++ Backend/UserModule/routes/api.php
@@ -6,19 +6,43 @@ use App\Http\Controllers\OtherController\HoraireController;
 use App\Http\Controllers\OtherController\LangueController;
 use App\Http\Controllers\OtherController\MetierController;
 use App\Http\Controllers\OtherController\ZoneServiceController;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Request;
+use App\Http\Requests\Api\V1\Annonce\public\SotreRequest;
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', [Usercontroller::class, 'login']);
 Route::prefix('register')->group(
     function () {
         Route::post('client', [Usercontroller::class, 'ClientRegister']);
-        Route::post('artisan', [Usercontroller::class, 'ArtisanRegister']);//no
+        Route::post('artisan', [Usercontroller::class, 'ArtisanRegister']); //no
     }
 );
 Route::prefix('user')->middleware(['auth:sanctum'])->group(function () {
     Route::post('/logout', [Usercontroller::class, 'logout']);
-    Route::get('/profil', [Usercontroller::class, 'profil']);
-    Route::get('/profil/update', [Usercontroller::class, 'profil']); //no
+    Route::get('/me', [Usercontroller::class, 'profil']);
+    Route::get('/me/update', [Usercontroller::class, 'profil']); //no
+});
+// Route::group(['prefix' => 'user/', 'middleware' => ['auth:sanctum']], function () {
+Route::group(['prefix' => 'user/'], function () {
+    Route::get('{user_id}/public/annonces', function ($user_id) {
+       return $response = Http::get('http://127.0.0.1:8001/api/user/'.$user_id.'/public/annonces')->json();
+    });
+    Route::get('{user_id}/public/annonces/{annonce_public}', function ($user_id,$annonce_public) {
+      return  $response = Http::get('http://127.0.0.1:8001/api/user/'.$user_id.'/public/annonces/'.$annonce_public)->json();
+    });
+    Route::post('{user_id}/public/annonces', function (SotreRequest $request,$user_id) {
+       return $response = Http::post('http://127.0.0.1:8001/api/user/'.$user_id.'/public/annonces/',$request->validated())->json();
+    });
+    Route::put('{user_id}/public/annonces/{annonce_public}', function (SotreRequest $request, $user_id,$annonce_public) {
+        return $response = Http::put('http://127.0.0.1:8001/api/user/'.$user_id.'/public/annonces/'.$annonce_public,$request->validated())->json();
+    });
+    Route::delete('{user_id}/public/annonces/{annonce_public}', function ($user_id,$annonce_public) {
+        return $response = Http::delete('http://127.0.0.1:8001/api/user/'.$user_id.'/public/annonces/'.$annonce_public)->json();
+    });
+    Route::get('{user_id}/public/annonces/{annonce_public}/ispublier', function ($user_id,$annonce_public) {
+       return $response = Http::get('http://127.0.0.1:8001/api/user/'.$user_id.'/public/annonces/'.$annonce_public)->json();
+    });
 });
 
 
@@ -40,3 +64,9 @@ Route::prefix('public')->group(function () {
     Route::apiResource('langues', LangueController::class)->only('index');
     Route::apiResource('zoneServices', ZoneServiceController::class)->only('index');
 });
+
+
+    // Route::post('/', function (Request $request) {
+    //     $response = Http::withToken($request->bearerToken())->post('http://service-a:80/api/entities', $request->all());   
+    //     return $response->json();
+    // }); 
